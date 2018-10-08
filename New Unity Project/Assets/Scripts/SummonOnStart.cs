6a19c0b461c80534d24c1b9bcd83167b481140ee
94tiger <94tiger@naver.com>
2018-10-08 11:13:14 +0900

    캐릭터 선택창 추가
    로비 내 캐릭터 선택창 추가

---
+++ New Unity Project/Assets/Scripts/SummonOnStart.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonOnStart : MonoBehaviour {
+    int charactors;
+    public GameObject []sHero = new GameObject[4];
+    GameObject respawn = null;
+
+	// Use this for initialization
+	void Start () {
+        charactors = TurnOnTheStage.charactorNum;
+        respawn = GameObject.FindGameObjectWithTag("Respawn");
+        // 위치를 위한 오브젝트
+        for(int i=0; i<4; i++)
+            if (charactors == i) Instantiate(sHero[i],respawn.transform.position , Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
